74534fd580c67f95227a509b280c08745d0d81b3
Dom Udall <dom@connected-ventures.com>
2018-06-09 13:49:02 +0100

    Use envvars for Google variables

--- gcs/rsync.sh
+++ gcs/rsync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo $1 > ${HOME}/gcp-key.json
+echo ${GOOGLE_AUTH} > ${HOME}/gcp-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcp-key.json
-gcloud --quiet config set project $2
-gcloud --quiet config set compute/zone $3
-gsutil rsync -d -r $4 $5
+gcloud --quiet config set project ${GOOGLE_PROJECT_ID}
+gcloud --quiet config set compute/zone ${GOOGLE_COMPUTE_ZONE}
+gsutil rsync -d -r $1 $2
