11dedb3e9ccc8d6aa2d3be5845a6697681f71d5a
Dom Udall <dom@connected-ventures.com>
2018-06-09 14:34:41 +0100

    Use printf instead of echo for more consistent output

--- gcs/rsync.sh
+++ gcs/rsync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo ${GOOGLE_AUTH} > ${HOME}/gcp-key.json
+printf "%s" "$GOOGLE_AUTH" > ${HOME}/gcp-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcp-key.json
 gcloud --quiet config set project ${GOOGLE_PROJECT_ID}
 gcloud --quiet config set compute/zone ${GOOGLE_COMPUTE_ZONE}
